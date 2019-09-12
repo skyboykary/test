6c9a8f684369fc6c199d9953bba2163908914158
chutaotao <chutaotao@variflight.com>
2019-09-12 17:29:55 +0800

    初始化

---
+++ demo/Test.php
@@ -0,0 +1,15 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+
+class Test extends GOMS_Controller {
+    
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    
+    public function test()
+    {
+    	var_dump(123);die;
+    }
+}
